924bbaf4425524b5916e78fa090a05fee245514c
Alberto <albalday@gmail.com>
2026-09-20 11:10:38 +0200

    fix: localizar termux-open-url desde el venv

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * y actualización en segundo plano (Stale-While-Revalidate).
  */
 
-const CACHE_NAME = 'zerochat-v7.1.4';
+const CACHE_NAME = 'zerochat-v7.1.5';
 
 const PRECACHE_ASSETS = [
   './',
