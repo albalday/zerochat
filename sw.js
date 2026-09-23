24c6d55c5d5017304dbefe5b263e91c35863e1e9
Alberto <albalday@gmail.com>
2026-09-23 22:33:05 +0200

    chore: bump version to 7.9.0

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * y actualización en segundo plano (Stale-While-Revalidate).
  */
 
-const CACHE_NAME = 'zerochat-v7.8.1';
+const CACHE_NAME = 'zerochat-v7.9.0';
 
 const PRECACHE_ASSETS = [
   './',
